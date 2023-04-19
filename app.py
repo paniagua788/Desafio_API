c64b17bb5c63f9b0062f9ab57443c2286d173003
Lucas Paniagua <lucas_paniagua@hotmail.com>
2023-04-19 16:55:57 -0400

    API funcionando localmente

--- app.py
+++ app.py
@@ -1,35 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from datetime import datetime
+from flask import Flask, jsonify, request
 
-# Hago un GET a la página de los valores UF y guardo la salida HTML en una variable 'r'
-r = requests.get("https://www.sii.cl/valores_y_fechas/uf/uf2022.htm")
+app = Flask(__name__)
 
-#print(r.text)
+@app.route('/uf', methods=['GET'])
+def obtener_UF():
+    # Obtengo el parámetro fecha de la url
+    fecha =request.args.get("fecha")
+    fecha_dt= datetime.strptime(fecha, "%Y-%m-%d")
+    anio= fecha_dt.year
+    dia= fecha_dt.day
+    mes= fecha_dt.month
+    if fecha_dt > datetime.today():
+        print("Fecha inválida")
+        return
 
-# Creo una instancia de BeautifulSoup pasandole el HTML y especificandole el tipo de parseo (HTML.parser)
-soup = BeautifulSoup(r.text, 'html.parser')
+    # Hace un GET a la página de los valores UF y guardo la salida HTML en una variable 'r'
+    r = requests.get(f"https://www.sii.cl/valores_y_fechas/uf/uf{anio}.htm")
 
-# Encuentra la tabla correspondiente en el HTML
-tabla_uf = soup.find('table', {'id': 'table_export'})
+    # Se crea una instancia de BeautifulSoup pasandole el HTML y especificandole el tipo de parseo (HTML.parser)
+    soup = BeautifulSoup(r.text, 'html.parser')
 
+    # Encuentra la tabla correspondiente en el HTML
+    tabla_uf = soup.find('table', {'id': 'table_export'})
 
+    # Encuentra la fila de la tabla que contiene el valor de UF buscado
+    fila_uf = tabla_uf.find('th', string= dia).find_parent('tr')
 
-# Encuentra la fila de la tabla que contiene el valor de UF buscado
-fila_uf = tabla_uf.find('th', string='29').find_parent('tr')
-#print(fila_uf)
+    #Encuentra todas las celdas de la fila
+    celdas = fila_uf.find_all('td')
 
+    #Encuentra el valor de UF específico en la fila
+    valor_uf = float(celdas[mes-1].get_text().replace('.', '').replace(',', '.'))
 
-#Encuentra todas las celdas de la fila
-celdas = fila_uf.find_all('td')
+    # Creamos un diccionario con la fecha y su valor UF correspondiente, para luego convertirlo a JSON y retornar
+    UF_info= {"Fecha": fecha, "Valor de UF" : valor_uf}
+    return jsonify(UF_info)
 
-#Encuentra el valor de UF específico en la fila
-valor_uf = float(celdas[0].get_text().replace('.', '').replace(',', '.'))
-
-print(valor_uf)
-
-#print(valor_uf)
-
-# Imprime el valor de UF
-#print(valor_uf)
-
+#Lanzamos la app Flask en el puerto 5000
+if __name__ == '__main__':
+    app.run(debug=True)
